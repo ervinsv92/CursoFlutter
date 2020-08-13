e8cd71b88dbc60fee3ab60a464b2cb74dd4743da
Ervin Solano Vargas <esolanov@grupocolono.com>
2020-08-13 16:14:34 -0600

    Base App QR

---
+++ qrreaderapp/lib/src/pages/home_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: _scanQR,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  _scanQR() async{
+
+    //https://fernando-herrera.com
+    //geo:40.68571377944765,-73.87685194453128
+
+    String futureString = '';
+
+    // try{
+    //   ScanResult scan = await BarcodeScanner.scan();
+    //   futureString = scan.rawContent;
+    // }catch(e){
+    //   e.toString();
+    // }
+
+    // print('Future string: $futureString');
+    // if(futureString != null){
+    //   print('Tenemos información');
+    // }
+  }
+
+  Widget _crearBottomNavigationBar(){
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+         BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        ),
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual){
+    switch(paginaActual){
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+      default: 
+        return MapasPage();
+    }
+  }
+}
